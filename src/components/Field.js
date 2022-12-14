38b203244f1c6d1cab45b6d1219ff11df6bab9e3
Marlon Almino <alminomarlon@gmail.com>
2022-12-14 11:20:15 -0300

    Componente Mina (Mine)

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import params from '../params'
+import Mine from './Mine'
 
 export default props => {
-  const { mined, opened, nearMines } = props
+  const { mined, opened, nearMines, exploded } = props
 
   const styleField = [styles.field]
   if (opened) styleField.push(styles.opened)
+  if (exploded) styleField.push(styles.exploded)
   if (styleField.length === 1) styleField.push(styles.regular)
 
   let color = null
@@ -23,6 +25,7 @@ export default props => {
         ? <Text style={[styles.label, { color: color }]}>{nearMines}</Text>
         : false
       }
+      {mined && opened ? <Mine /> : false}
     </View>
   )
 }
@@ -49,5 +52,9 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: 'bold',
     fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red'
   }
 })
