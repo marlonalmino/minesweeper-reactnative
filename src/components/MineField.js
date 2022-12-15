ac6e79b6be82484c2e991bd429a0a7a672238a79
Marlon Almino <alminomarlon@gmail.com>
2022-12-15 19:10:48 -0300

    Correção no Componente MineField

--- src/components/MineField.js
+++ src/components/MineField.js
@@ -7,14 +7,13 @@ export default props => {
     const columns = row.map((field, f) => {
       return <Field {...field} key={f} />
     })
-    return <View key={r}>{columns}</View>
+    return <View key={r} style={{ flexDirection: 'row' }}>{columns}</View>
   })
   return <View style={styles.container}>{rows}</View>
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
     backgroundColor: '#EEE'
   }
 })
