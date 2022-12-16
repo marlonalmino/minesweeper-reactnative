7d863de918cf846b8a5cb8021b3422675c3ffbf3
Marlon Almino <alminomarlon@gmail.com>
2022-12-16 10:14:32 -0300

    Lógica do Jogo #02

--- src/functions.js
+++ src/functions.js
@@ -36,4 +36,74 @@ const createMinedBoard = (rows, columns, minesAmount) => {
   return board
 }
 
-export { createMinedBoard }
+const cloneBoard = board => {
+  return board.map(rows => {
+    return rows.map(field => {
+      return { ...field }
+    })
+  })
+}
+
+const getNeighbors = (board, row, column) => {
+  const neighbors = []
+  const rows = [row - 1, row, row + 1]
+  const columns = [column - 1, column, column + 1]
+
+  rows.forEach(r => {
+    columns.forEach(c => {
+      const different = r !== row || c !== column
+      const validRow = r >= 0 && r < board.length
+      const validColumn = c >= 0 && c < board[0].length
+
+      if (different && validRow && validColumn) {
+        neighbors.push(board[r][c])
+      }
+    })
+  })
+  return neighbors
+}
+
+const safeNeighborhood = (board, row, column) => {
+  const safes = (result, neighbor) => result && !neighbor.mined
+  return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+const openField = (board, row, column) => {
+  const field = board[row][column]
+  if (!field.opened) {
+    field.opened = true
+    if (field.mined) {
+      field.exploded = true
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column)
+        .forEach(n => openField(board, n.row, n.column))
+    } else {
+      const neighbors = getNeighbors(board, row, column)
+      field.nearMines = neighbors.filter(n => n.mined).length
+    }
+  }
+}
+
+const fields = board => [].concat(...board)
+
+const hadExplosion = board => fields(board)
+  .filter(field => field.exploded).length > 0
+
+const pending = field => (field.mined && !field.flagged)
+  || (!field.mined && !field.opened)
+
+const wonGame = board => fields(board).filter(pending).length === 0
+
+const showMines = board => fields(board)
+  .filter(field => field.mined)
+  .forEach(field => field.opened = true)
+
+
+export {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines
+}
