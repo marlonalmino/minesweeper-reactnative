cae38bb8629b71bf75c4c7d8d44d27493a05dff5
Marlon Almino <alminomarlon@gmail.com>
2022-12-17 12:05:05 -0300

    Abrir Campo (onPress)

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Alert } from 'react-native'
 import params from './src/params'
 import MineField from './src/components/MineField'
-import { createMinedBoard } from './src/functions'
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+} from './src/functions'
 
 export default class App extends Component {
 
@@ -22,7 +29,27 @@ export default class App extends Component {
     const rows = params.getRowsAmount()
     return {
       board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
     }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeeeeu!', 'Que buuuurro!')
+    }
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, lost, won })
   }
 
   render() {
@@ -33,7 +60,7 @@ export default class App extends Component {
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
         <View style={styles.board}>
-          <MineField board={this.state.board} />
+          <MineField board={this.state.board} onOpenField={this.onOpenField} />
         </View>
       </View>
     )
--- src/components/MineField.js
+++ src/components/MineField.js
@@ -5,7 +5,8 @@ import Field from './Field'
 export default props => {
   const rows = props.board.map((row, r) => {
     const columns = row.map((field, f) => {
-      return <Field {...field} key={f} />
+      return <Field {...field} key={f}
+        onOpen={() => props.onOpenField(r, f)} />
     })
     return <View key={r} style={{ flexDirection: 'row' }}>{columns}</View>
   })
